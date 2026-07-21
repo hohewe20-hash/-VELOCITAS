e0b90311248090c2cf4e1b29f5e781aa99ab1083
hohewe20-hash <hohewe20@gmail.com>
2026-07-21 03:19:43 +0300

    SS

--- frontend/src/data/cars.js
+++ frontend/src/data/cars.js
@@ -848,7 +848,7 @@ export const cars = [
     price: 274500,
     category: 'Sport',
     image: 'https://1cars.org/wp-content/uploads/2021/02/BMW-M3-Competition-3.jpg',
-    heroImage: 'https://www.youtube.com/watch?v=YAFUyPp_238',
+    heroVideo: "https://www.youtube.com/watch?v=YAFUyPp_238",
     color: 'Flux Orange',
     tagline: 'Eat the track.',
     specs: {
